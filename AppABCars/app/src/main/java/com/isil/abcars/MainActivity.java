d34dbf33bac89976772f94671acd15a91431af8e
Marco Tejeda <jeanmarcotejeda@gmail.com>
2016-11-20 15:35:50 -0500

    Se agrego el DrawerLayout

--- AppABCars/app/src/main/java/com/isil/abcars/MainActivity.java
+++ AppABCars/app/src/main/java/com/isil/abcars/MainActivity.java
@@ -1,82 +1,121 @@
 package com.isil.abcars;
 
-import android.content.Context;
 import android.content.Intent;
-import android.support.design.widget.Snackbar;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.Button;
-import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.isil.abcars.entity.PostEntity;
-import com.isil.abcars.presenter.PostPresenter;
-import com.isil.abcars.presenter.PostView;
 import com.isil.abcars.storage.PreferencesHelper;
 import com.isil.abcars.view.adapters.PostAdapter;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity{
 
     private static final String TAG ="MainActivity";
 
-    private TextView txtLogout,txtBienvenida;
-    private ListView listPost;
     private PostAdapter postAdapter;
+    private Toolbar toolbar;
+    private NavigationView navigationView;
+    private DrawerLayout drawerLayout;
 
-    private ArrayList<PostEntity> arreglo;
+    private TextView txtUsuario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        arreglo = new ArrayList<PostEntity>();
 
-        txtLogout = (TextView)findViewById(R.id.txtLogout);
-        txtBienvenida = (TextView)findViewById(R.id.txtBienvenida);
-        listPost = (ListView)(findViewById(R.id.listPost));
+        //Toolbar
+        toolbar = (Toolbar) findViewById(R.id.navigationToolbar);
+        setSupportActionBar(toolbar);
 
-        //User Bienvenida
-        String us = PreferencesHelper.getUserSession(this);
-        if(us != null) {
-            txtBienvenida.setText("Bienvenido "+ us);
-        }
+        //Initializing NavigationView
+        navigationView = (NavigationView) findViewById(R.id.navigationView);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
 
-        txtLogout.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                logout();
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+
+                //Checking if the item is in checked state or not, if not make it in checked state
+                if (menuItem.isChecked()) menuItem.setChecked(false);
+                else menuItem.setChecked(true);
+
+                //Closing drawer on item click
+                drawerLayout.closeDrawers();
+
+                //Check to see which item was being clicked and perform appropriate action
+                switch (menuItem.getItemId()) {
+
+                    case R.id.menuPerfil:
+                        Toast.makeText(getApplicationContext(),"Perfil Selected",Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.menuLisatdo:
+                        Toast.makeText(getApplicationContext(),"Listado Selected",Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.menuSesion:
+                        logout();
+                        return true;
+                    default:
+                        return true;
+
+                }
             }
+
         });
 
+        // Initializing Drawer Layout and ActionBarToggle
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar, R.string.openDrawer, R.string.closeDrawer) {
 
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                /* Code here will be triggered once the drawer closes as we dont want anything
+                to happen so we leave this blank */
+                super.onDrawerClosed(drawerView);
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                /* Code here will be triggered once the drawer open as we dont want anything to
+                happen so we leave this blank */
+                super.onDrawerOpened(drawerView);
+            }
+        };
+
+        //Setting the actionbarToggle to drawer layout
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+
+        //calling sync state is necessay or else your hamburger icon wont show up
+        actionBarDrawerToggle.syncState();
+
+        //Setting the userEmail on herderLayout
+        View headerView = navigationView.getHeaderView(0);
+        txtUsuario = (TextView) headerView.findViewById(R.id.txtUsuario);
+        String us = PreferencesHelper.getUserSession(this);
+        if(us != null) {
+            txtUsuario.setText("Bienvenido "+ us);
+        }
 
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-
-        PostEntity pe1 = new PostEntity(10000.00, "descripcion 1");
-        PostEntity pe2 = new PostEntity(13000.00, "descripcion 2");
-        PostEntity pe3 = new PostEntity(14000.00, "descripcion 3");
-        PostEntity pe4 = new PostEntity(20000.00, "descripcion 4");
-
-        arreglo.add(pe1);
-        arreglo.add(pe2);
-        arreglo.add(pe3);
-        arreglo.add(pe4);
-
-        postAdapter = new PostAdapter(this,arreglo);
-        listPost.setAdapter(postAdapter);
-
-
     }
+
+    /**
+     * Metodos privados
+     ___________________________________________________________________________________________ */
 
     private void logout() {
         PreferencesHelper.signOut(this);
